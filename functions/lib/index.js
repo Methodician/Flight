e0c4aab8dbe95c7895c0014d1291cb2ae1c44fa7
Methodician <jacobgjohnston@gmail.com>
2017-12-18 10:06:22 -0800

    functions working had to re-initialize without tslint

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,7 +4,14 @@ const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+exports.emailContactSubmission = functions.firestore
+    .document('contacts/{pushId}')
+    .onCreate(e => {
+    const contactInfo = e.data.data();
+    console.log(contactInfo);
+    return contactInfo;
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,14 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+export const emailContactSubmission = functions.firestore
+    .document('contacts/{pushId}')
+    .onCreate(e => {
+        const contactInfo = e.data.data();
+        console.log(contactInfo);
+        return contactInfo;
+    });
